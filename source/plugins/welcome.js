bb8ea82505b1efc2e05f47a768b67474fd7baccb
Sean McNamara <Sean McNamara smcnam@gmail.com>
2013-08-11 14:18:51 -0400

    Customize the bot for the Root Access chat.

--- source/plugins/welcome.js
+++ source/plugins/welcome.js
@@ -1,27 +1,44 @@
 (function () {
 "use strict";
-//welcomes new users with a link to the room rules
+//welcomes new users with a link to the room rules - SuperUser implementation
 
 var seen = bot.memory.get( 'users' );
 
-var message = "Welcome to the JavaScript chat! Please review the " +
-		bot.adapter.link(
-			"room pseudo-rules",
-			"http://rlemon.github.com/so-chat-javascript-rules/" ) + ". " +
+var message = "Welcome to Root Access chat for " + bot.adapter.link("Super User", "http://superuser.com") + "s! " +
 	"Please don't ask if you can ask or if anyone's around; just ask " +
 	"your question, and if anyone's free and interested they'll help.";
+	
+var messageForSpecialPeople = "Hey there! You're pretty special, aren't you? :)";
 
-function welcome ( name, room ) {
+var messageForOwner = "Oh no, master is here! Hide the cookies!";
+
+function welcome ( user, room ) {
+	var msg = null;
+	if(bot.isOwner( user.id ))
+	{
+		msg = messageForOwner;
+	}
+	else if(user.name >= 20 && user.name <= 10000)
+	{
+		msg = message;
+	}
+	else
+	{
+		msg = messageForSpecialPeople;
+	}
+	
+	bot.adapter.out.add(
+		bot.adapter.reply( user.name ) + " " + msg, room );
+}
+
+function welcomeDirect ( name, room ) {
 	bot.adapter.out.add(
 		bot.adapter.reply( name ) + " " + message, room );
 }
 
 IO.register( 'userregister', function ( user, room ) {
-	var semiLegitUser = bot.isOwner( user.id ) ||
-		user.reputation > 1000 || user.reputation < 20;
-
 	if (
-		Number( room ) !== 17 || semiLegitUser  || seen[ user.id ]
+		Number( room ) !== 118  || seen[ user.id ]
 	) {
 		if ( semiLegitUser ) {
 			finish( true );
@@ -40,12 +57,12 @@ IO.register( 'userregister', function ( user, room ) {
 		//I'm parsing html with regexps. hopefully Cthulu won't eat me.
 		// <a href="/transcript/17">7</a>
 		// <a href="/transcript/17">47.1k</a>
-		var chatMessages = /transcript\/17(?:'|")>([\d\.]+)(k?)/.exec( resp );
+		var chatMessages = /transcript\/118(?:'|")>([\d\.]+)(k?)/.exec( resp );
 
 		if ( !chatMessages || (
 			!chatMessages[ 2 ] || parseFloat( chatMessages[1] ) < 2
 		)) {
-			welcome( user.name, room );
+			welcome( user, room );
 		}
 		finish();
 	}
@@ -68,7 +85,7 @@ bot.addCommand({
 			return message;
 		}
 
-		welcome( args, args.get('roomid') );
+		welcomeDirect( args, args.get('roomid') );
 	},
 	permission : {
 		del : 'NONE'
