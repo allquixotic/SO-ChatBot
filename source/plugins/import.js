79796238e22c0ad1c8119359963f31ef66e2275c
Sean McNamara <smcnam@gmail.com>
2013-12-31 09:27:28 -0500

    Update import.js
    xhr as a solution to huge size

--- source/plugins/import.js
+++ source/plugins/import.js
@@ -7,9 +7,15 @@
 
   bot.addCommand(bot.CommunityCommand({
     name : 'import',
-    fun : function () { 
-      bot.memory.data = JSON.parse(b64_to_utf8(args.replace(/\s/g,"")));
-      bot.memory.save();
+    fun : function (args) { 
+      var request = new XMLHttpRequest();
+      request.open('GET', args, false);
+      request.send(null);
+      if (request.status === 200) {
+        bot.memory.data = JSON.parse(b64_to_utf8(request.responseText.replace(/\s/g,"")));
+        bot.memory.save();
+      }
+      
       return "Imported and persisted successfully";
     },
     permissions : { del : 'NONE', use : 'OWNER' },
